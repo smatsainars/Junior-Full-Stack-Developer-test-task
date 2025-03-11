00d253e74644ffb4a4e63afc74f502cadb6e455f
smatsainars <smats.ainars@gmail.com>
2025-03-11 14:51:19 +0200

    Update GraphQL endpoint URI in main.tsx for production environment

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  uri: 'https://juniorfullstackdevelopertesttask.shop/graphql',
   cache: new InMemoryCache()
 });
 
